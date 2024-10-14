62ac199f9df2173fdf4d221a8ad89d483da827d6
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-14 22:48:00 +0300

    в процессе

--- basics_1-67.html/20-30/script_1.js
+++ basics_1-67.html/20-30/script_1.js
@@ -19,4 +19,13 @@ alert('ЗАДАНИЕ 26.1:'),alert(a ** 10);
 let fname = 'Enver', lname = 'Dzheparov';
 alert('ЗАДАНИЕ 28.1:'),alert(fname), alert(lname);
 
+let str = '!!!';
+alert('ЗАДАНИЕ 29.1:'),alert(str);
 
+let str1 = 'java', str2 = 'script';
+alert('ЗАДАНИЕ 29.2:'),alert(str1 + str2);
+str1 = 'hello', str2 = 'world';
+alert('ЗАДАНИЕ 29.3:'),alert(str1 +' '+ str2);
+
+str='gdz_po_jizn';
+alert('ЗАДАНИЕ 30:'),alert('gdz_po_jizn'.length);
