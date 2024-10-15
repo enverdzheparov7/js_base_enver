7bb850af68f68304c3f605072f96423aac2bd56d
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-15 13:46:48 +0300

    в процессе

--- basics_1-67.html/30-40/script_2.js
+++ basics_1-67.html/30-40/script_2.js
@@ -1,23 +1,28 @@
 //31
-let str3 = 'xxx';
-let str4 = 'yyy';
-let txt  = `aaa  ${str1} bbb ${str2} ccc`;
+//let str3 = 'xxx';
+//let str4 = 'yyy';
+//let txt  = `aaa  ${str1} bbb ${str2} ccc`;
 //32
+alert('ЗАДАНИЕ 32:');
 let str_32 = `a
 b
 c
 `;
 alert(str_32);
-//33
+alert('ЗАДАНИЕ 34:');
+//34
 let b = undefined;
 alert(b);
-
-//34
+alert('ЗАДАНИЕ 35:');
+//35
 let aa = null;
 alert(aa);
-
-//35
+alert('ЗАДАНИЕ 36:');
+//36
 let ab = true;
 alert(ab);
 let bc = false;
 alert(bc);
+//37
+alert('ЗАДАНИЕ 37:');
+alert(NaN * 1);
