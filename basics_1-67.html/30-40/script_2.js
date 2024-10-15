b845fd1bbd086571ef2e7054f6e78c8d604bb60b
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-15 13:43:14 +0300

    в процессе

--- basics_1-67.html/30-40/script_2.js
+++ basics_1-67.html/30-40/script_2.js
@@ -3,7 +3,21 @@ let str3 = 'xxx';
 let str4 = 'yyy';
 let txt  = `aaa  ${str1} bbb ${str2} ccc`;
 //32
-let str_res_32 = `a
+let str_32 = `a
 b
 c
 `;
+alert(str_32);
+//33
+let b = undefined;
+alert(b);
+
+//34
+let aa = null;
+alert(aa);
+
+//35
+let ab = true;
+alert(ab);
+let bc = false;
+alert(bc);
