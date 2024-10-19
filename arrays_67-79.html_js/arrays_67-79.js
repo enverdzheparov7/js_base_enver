dd7503f57bfb09e06abe66661ada1f786c824ebf
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-19 15:04:44 +0300

    в процессе

--- arrays_67-79.html_js/arrays_67-79.js
+++ arrays_67-79.html_js/arrays_67-79.js
@@ -7,3 +7,27 @@ let arr_2 = ['Wer' + 'ner ' + 'Chelsea'];
 alert(arr_2);
 console.log(arr_2);
 
+alert(`ЗАДАНИЕ 69.1:`);
+
+let arr_3 = ['1', '2', '3'];
+
+alert(arr_3[0]); // выведет '1'
+alert(arr_3[1]); // выведет '3'
+alert(arr_3[2]); // выведет '3'
+
+alert(`ЗАДАНИЕ 69.2:`);
+
+let arr_4 = ['1', '2', '3'];
+
+alert(arr_4[0]); 
+alert(arr_4[1]); 
+alert(arr_4[2]); 
+
+alert(`ЗАДАНИЕ 69.3:`);
+
+alert(arr[0] + arr[1] + arr[2] )
+
+alert(`ЗАДАНИЕ 69.4:`);
+
+arr = ['a', 'b', 'c', 'd'];
+alert("'" + arr[0] +'+'+ arr[1] +'+'+ arr[2] +'+'+ arr[3]+"'");
