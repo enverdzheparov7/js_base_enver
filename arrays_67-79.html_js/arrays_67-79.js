bc88af7f01dca103dc1bb6999980186f5803b00c
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-19 17:43:10 +0300

    в процессе

--- arrays_67-79.html_js/arrays_67-79.js
+++ arrays_67-79.html_js/arrays_67-79.js
@@ -1,61 +1,75 @@
-alert(`ЗАДАНИЕ 68:`)
+
 let arr = [1, 2, 3];
-alert(arr);
+
 console.log(arr);
+alert(`ЗАДАНИЕ 68.1:` + '\n' + arr)
+
 
 let arr_2 = ['Wer' + 'ner ' + 'Chelsea'];
-alert(arr_2);
+alert(`ЗАДАНИЕ 68.2:` + '\n' + arr_2)
 console.log(arr_2);
 
-alert(`ЗАДАНИЕ 69.1:`);
 
 let arr_3 = ['1', '2', '3'];
+alert(`ЗАДАНИЕ 69.1:` + '\n' + arr_3[0] + '\n' + arr_3[1]  + '\n' + arr_3[2]  );
 
-alert(arr_3[0]); // выведет '1'
-alert(arr_3[1]); // выведет '3'
-alert(arr_3[2]); // выведет '3'
 
-alert(`ЗАДАНИЕ 69.2:`);
+
+
 
 let arr_4 = ['1', '2', '3'];
 
-alert(arr_4[0]); 
-alert(arr_4[1]); 
-alert(arr_4[2]); 
+alert(`ЗАДАНИЕ 69.2:` + '\n' + arr_4[0] + '\n' + arr_4[1]  + '\n' + arr_4[2]  );
 
-alert(`ЗАДАНИЕ 69.3:`);
+alert(`ЗАДАНИЕ 69.3:` + '\n' + arr[0] + arr[1] + arr[2]);
 
-alert(arr[0] + arr[1] + arr[2] )
 
-alert(`ЗАДАНИЕ 69.4:`);
 
 arr = ['a', 'b', 'c', 'd'];
-alert("'" + arr[0] +'+'+ arr[1] +'+'+ arr[2] +'+'+ arr[3]+"'");
 
-alert(`ЗАДАНИЕ 70.1:`);
+alert(`ЗАДАНИЕ 69.4:` + '\n' + "'" + arr[0] +'+'+ arr[1] +'+'+ arr[2] +'+'+ arr[3]+"'");
+
 arr = ['a', 'b', 'c', 'd'];
-alert(arr.length);
+alert(`ЗАДАНИЕ 70.1:`+ '\n'+ arr.length);
 
-alert(`ЗАДАНИЕ 70.2:`);
-alert(arr[arr.length - 1]);
+alert(`ЗАДАНИЕ 70.2:` + '\n' + arr[arr.length - 1]);
 
-alert(`ЗАДАНИЕ 71:`);
+
 arr = ['a', 'b', 'c'];
 arr[0] = '1';
 arr[1] = '2';
 arr[2] = '3';
 console.log(arr); 
-alert(arr);
+alert(`ЗАДАНИЕ 71:`+ '\n' + arr);
 
-alert(`ЗАДАНИЕ 72:`);
+
 arr = [1, 2, 3];
 arr[0] += 3;
 arr[1] += 3;
 arr[2] += 3;
-alert(arr);
-alert(`ЗАДАНИЕ 73:`);
+alert(`ЗАДАНИЕ 72:`+ '\n' +  arr);
+
 arr = [1, 2, 3];
 ++arr[0];
 ++arr[1];
 ++arr[2];
-alert(arr);
+alert(`ЗАДАНИЕ 73:` + '\n' + arr);
+
+
+let arr1 = [];
+
+arr1[0] = '1';
+arr1[1] = '2';
+arr1[2] = '3';
+
+alert(`ЗАДАНИЕ 74.1:` + '\n' + arr1);
+
+arr1 = [1, 2, 3];
+
+
+arr1[3] = '4';
+arr1[4] = '5';
+
+
+alert(`ЗАДАНИЕ 74.2:` + '\n' + arr1);
+
