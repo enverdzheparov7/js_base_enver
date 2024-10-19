42c65cff543b03ee5d05ffbdda32002efd28642c
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-19 23:09:38 +0300

    в процессе

--- objects_80-104.html_js/objects_92-104.js
+++ objects_80-104.html_js/objects_92-104.js
@@ -19,3 +19,9 @@ let [key3] = 'z';
 console.log(obj);
 
 alert('ЗАДАНИЕ 94: выведет true и false сответственно'); 
+
+alert('ЗАДАНИЕ 95: false ');
+
+alert(`ЗАДАНИЕ 96: 
+    1-3. object 
+    4. number `);
