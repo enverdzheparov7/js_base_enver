7d9a36436d8344d00b4235c48a27d60ab86d8b74
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-19 23:05:45 +0300

    в процессе

---
+++ objects_80-104.html_js/objects_92-104.js
@@ -0,0 +1,21 @@
+//93
+
+let key = 'x';
+
+let obj = {
+	[key]: 1,
+	y: 2,
+	z: 3
+};
+
+let obj1 = {
+	x: 1,
+	y: 2,
+	z: 3
+};
+let [key1] = 'x';
+let [key2] = 'y';
+let [key3] = 'z';
+console.log(obj);
+
+alert('ЗАДАНИЕ 94: выведет true и false сответственно'); 
