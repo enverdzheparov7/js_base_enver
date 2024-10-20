a3340cee284696ee16db4c6b96307af4043eff40
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-20 11:22:31 +0300

    в процессе

--- cycles_104-142.html_js/cycles_104-142.js
+++ cycles_104-142.html_js/cycles_104-142.js
@@ -36,3 +36,26 @@ if (test != 10) {
 } else {
 	alert('107 ' +'---'); // сработает это
 }
+
+let test1 = 12;
+let test2 = 13;
+
+if (test1 > test2) {
+	alert('108.1 ' + '+++'); 
+} else {
+	alert('108.1 ' +'---'); 
+}
+
+if (test1 == test2) {
+	alert('108.2 ' + '+++'); 
+} else {
+	alert('108.2 ' +'---'); 
+}
+
+let test3 = 'afk';
+let test4 = 'afk';
+if (test3 == test4) {
+	alert('109 ' + '+++'); 
+} else {
+	alert('109 ' +'---'); 
+}
