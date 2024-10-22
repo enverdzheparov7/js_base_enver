286e06e42d4aacc021c90f3674dbae6a1053d62f
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-22 16:05:54 +0300

    в процессе

--- cycles_143-167.html_js/cycles_143-167.js
+++ cycles_143-167.html_js/cycles_143-167.js
@@ -15,9 +15,59 @@ for(let key in obj){
 }
 //146
 
+//146
+
 let num = 1;
-while(num>100){
-    nim++;
+while(num<100){
+    num++;
     console.log(num);
 }
 
+let num1 = 11;
+
+while(num1<33){
+    num1++;
+    console.log(num1)
+}
+
+let i = 0;
+let num2 = 3;
+
+while(num2<1000){
+    num2*=3;
+    console.log(num2)
+    ++i;
+}
+
+console.log('Количество итераций - ' + i)
+
+//147
+
+for(let i = 0; i<=100; ++i)
+{
+    console.log(i);
+}
+
+for(let i = 11; i<=33; ++i)
+    {
+        console.log(i);
+    }
+
+    for(let i = 0; i<=100; ++i)
+        {
+            if(i%2==0){
+                console.log(i);
+            }
+        }
+        for(let i = 0; i<=99; ++i)
+            {
+                if(i%2==1){
+                    console.log(i);
+                }
+            }
+
+            for(let i=100; i>=0; i-- ){
+                console.log(i);
+            }
+        
+    
