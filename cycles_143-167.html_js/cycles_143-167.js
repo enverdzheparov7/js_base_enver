64a9bd124063641bb0ecc574397c75d2f6fff722
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-22 16:32:15 +0300

    в процессе

--- cycles_143-167.html_js/cycles_143-167.js
+++ cycles_143-167.html_js/cycles_143-167.js
@@ -70,4 +70,47 @@ for(let i = 11; i<=33; ++i)
                 console.log(i);
             }
         
-    
+    //148
+   //1
+    arr = ['a', 'b', 'c', 'd', 'e'];
+
+    for (let i; i < arr.length; i++){
+        console.log(arr[i])
+    }
+//2
+    for (let i = 1; i < arr.length -1; i++){
+        console.log(arr[i])
+    }
+
+///3
+    for (let i = arr.length - 1; i >= 0; i--){
+        console.log(arr[i])
+    }
+//4
+    arr = ['a', 'b', 'c', 'd', 'e'];
+		
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+}
+//149
+//1
+let arr1 = [2, 5, 9, 15, 1, 4];
+
+for(let i = 0; i < arr1.length; i++ )
+{
+    if(arr[i] > 3 && arr[i] <10){
+        console.log(arr[i])
+    }
+}
+
+//2
+
+obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+
+for(let key in obj){
+    if(obj[key]%2!=0)
+        console.log(obj[key])
+}
+
+   
+   
