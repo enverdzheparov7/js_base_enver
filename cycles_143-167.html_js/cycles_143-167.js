af9e5c9a1ec6a000cb96df3972915ed5121c4a5c
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-22 15:54:07 +0300

    в процессе

--- cycles_143-167.html_js/cycles_143-167.js
+++ cycles_143-167.html_js/cycles_143-167.js
@@ -4,5 +4,20 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 for(let elem of arr){
     console.log(elem);
 }
+//145
+let obj = {x: 1, y: 2, z: 3};
+for(let key in obj){
+    console.log(key)
+}
 
+for(let key in obj){
+    console.log(obj[key])
+}
+//146
+
+let num = 1;
+while(num>100){
+    nim++;
+    console.log(num);
+}
 
