02b143b3dcb2ebc5ca7bfbc910607116b217e12c
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-28 20:49:55 +0300

    в процессе

--- multiarr_167-183.html_js/multiarr_167-183.js
+++ multiarr_167-183.html_js/multiarr_167-183.js
@@ -383,3 +383,31 @@ console.log(`175
 	
 	console.log(arr);
 	`)
+
+	//180
+
+	let employees = [
+	{
+		name: 'name1',
+		salary: 300,
+		age: 28,
+	},
+	{
+		name: 'name2',
+		salary: 400,
+		age: 29,
+	},
+	{
+		name: 'name3',
+		salary: 500,
+		age: 30,
+	},
+];
+
+employees.push({
+		name: 'name4',
+		salary: 600,
+		age: 33,
+	})
+	
+	console.log(employees)
