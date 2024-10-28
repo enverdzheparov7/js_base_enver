8a678e534a231479949efbbf3a13b1bc0fabd2c9
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-28 21:50:40 +0300

    в процессе

--- multiarr_167-183.html_js/multiarr_167-183.js
+++ multiarr_167-183.html_js/multiarr_167-183.js
@@ -541,6 +541,117 @@ console.log(students['group3'][0])
 
 
 
+let data = {
+	1: [
+		'data11',
+		'data12',
+		'data13',
+	],
+	2: [
+		'data21',
+		'data22',
+		'data23',
+	],
+	3: [
+		'data31',
+		'data32',
+		'data33',
+	],
+	4: [
+		'data41',
+		'data42',
+		'data43',
+	],
+};
+
+//179
+for (let num in data) {
+	for (let name_date of data[num]) {
+		console.log(name_date);
+	}
+}
+//179_2
+let data1 = [
+	{
+		1: 'data11',
+		2: 'data12',
+		3: 'data13',
+	},
+	{
+		1: 'data21',
+		2: 'data22',
+		3: 'data33',
+	},
+	{
+		1: 'data31',
+		2: 'data32',
+		3: 'data33',
+	},
+];
+
+for (let num in data1) {
+  for (let name_date in data1[num]) {
+    // Получаем значение по ключу
+    let da = data1[num][name_date];
+    console.log(da); // Выводим значение
+  }
+}
+
+
+//3 
+
+
+let data2 = [
+	{
+		1: [
+			'data111',
+			'data112',
+			'data113',
+		],
+		2: [
+			'data121',
+			'data122',
+			'data123',
+		],
+	},
+	{
+		1: [
+			'data211',
+			'data212',
+			'data213',
+		],
+		2: [
+			'data221',
+			'data222',
+			'data223',
+		],
+	},
+	{
+		1: [
+			'data411',
+			'data412',
+			'data413',
+		],
+		2: [
+			'data421',
+			'data422',
+			'data423',
+		],
+	},
+];
+
+
+for (let num in data2) {
+  
+	for (let name_date in data2[num]) {
+	  let da = data2[num];
+	  for(let dater of da[name_date] ){
+		console.log(dater);
+	}
+}
+}
+
+
 	//182
 
 	let employees = [
