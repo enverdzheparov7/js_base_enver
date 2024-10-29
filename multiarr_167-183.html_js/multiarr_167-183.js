b89de5c8d36b277d0d3a0bceb2bfaa282ed2fa5e
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-10-29 14:05:24 +0300

    выполнено

--- multiarr_167-183.html_js/multiarr_167-183.js
+++ multiarr_167-183.html_js/multiarr_167-183.js
@@ -873,3 +873,60 @@ let obj = {
 console.log(obj['key2']['key4']);
 
 */
+
+
+
+//183 
+
+let affairs3 = {
+	'2019-12-28': ['data11', 'data12', 'data13'],
+	'2019-12-29': ['data21', 'data22', 'data23'],
+	'2019-12-30': ['data31', 'data32', 'data33'],
+}
+
+affairs3['2019-12-29'].push('data24');
+
+let newDate = '2019-12-31';
+let newData = ['data41', 'data42'];
+
+// Добавляем новую дату в объект
+affairs3[newDate] = newData;
+
+console.log(affairs3);
+
+
+//183_4
+
+
+let students2 = {
+	'group1': {
+		'subgroup11': ['student111', 'student112', 'student113'],
+		'subgroup12': ['student121', 'student122', 'student123'],
+	},
+	'group2': {
+		'subgroup21': ['student211', 'student212', 'student213'],
+		'subgroup22': ['student221', 'student222', 'student223'],
+	},
+	'group3': {
+		'subgroup31': ['student311', 'student312', 'student313'],
+		'subgroup32': ['student321', 'student322', 'student323'],
+	},
+};
+//5
+
+students2['group1']['subgroup11'].push('student114');
+
+
+//6 
+
+students2['group1']['subgroup13'] = ['student131', 'student132'];
+
+console.log(students2);
+
+//7
+students2['group4'] = {
+    'subgroup41': ['student411', 'student412', 'student413']
+};
+
+console.log(students2);
+
