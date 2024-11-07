d290149b0b6f1608ad7e7c168400a0a1dc154cf6
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-07 14:26:07 +0300

    task 243

--- nested_func_240-256.html_js/nested_func_240-256.js
+++ nested_func_240-256.html_js/nested_func_240-256.js
@@ -34,3 +34,47 @@ test1(
     func2 = function (){return 2;},
     func2 = function (){return 3;}
 ) 
+
+//242
+// 1 2
+test2(func5 = function(num) {
+	return num * num * num;
+});
+//3
+test2( function func6 (num) {
+	return num * num * num;
+});
+
+function test2(func) {
+	console.log(func(3));
+} 
+
+
+//4
+test3( function func7 (num1, num2) {
+	return num1 + num2;
+}); 
+
+function test3(func1) {
+	console.log(func1(2, 3)); 
+} 
+
+
+//243
+
+
+function test4(num, func1, func2) { 
+    return func1(num) + func2(num); 
+}
+
+
+let result = test4(3, function square(num) {
+    return num * num; 
+},
+
+function cube(num) {
+    return num * num * num; 
+});
+
+
+console.log(result);
