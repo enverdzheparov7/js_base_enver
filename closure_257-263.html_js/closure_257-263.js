9bd8382e9869cc57d276c3e36343afa788c8fd78
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-07 18:06:44 +0300

    выполнено

--- closure_257-263.html_js/closure_257-263.js
+++ closure_257-263.html_js/closure_257-263.js
@@ -92,3 +92,100 @@ func();
 func(); 
 func(); 
 func(); 
+
+//262 
+//1
+function func2() {
+	let num = 0;
+	
+	return function() {
+		console.log(num);
+		num++;
+	};
+}
+
+func2()();//0
+func2()();//0
+func2()();//0
+
+//2 
+function func40() {
+	let num = 0;
+	
+	return function() {
+		console.log(num);
+		num++;
+	};
+}
+
+let test11 = func40;
+
+test11()();//0
+test11()();//0
+test11()();//0
+
+//263
+
+//1
+
+let counter = 0;
+		
+function test33() {
+	return function() {
+		console.log(counter);
+		counter++;
+	};
+}
+
+let func12 = test33;
+
+let func11 = func12();
+let func20 = func12();
+func11();//0
+func20();//1
+func11();//2
+func20();//3
+
+//2
+
+function test4() {
+	let counter = 0;
+	
+	return function() {
+		return function() {
+			console.log(counter);
+			counter++;
+		};
+	};
+}
+
+let func22 = test4()();
+
+let func17 = func22;
+let func23 = func22;
+func17();//
+func22();//
+func17();//
+func22();//
+
+//3 
+
+function test263() {
+	let counter = 0;
+	
+	return function() {
+		return function() {
+			console.log(counter);
+			counter++;
+		};
+	};
+}
+
+let func263 = test263();
+
+let func1_263 = func263();
+let func2_263 = func263();
+func1_263();//0
+func2_263();//1
+func1_263();//2
+func2_263();//3
