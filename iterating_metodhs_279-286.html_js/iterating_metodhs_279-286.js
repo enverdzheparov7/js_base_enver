477ee0a48766fcfadc6889d0f648a238d9e47360
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-09 01:49:48 +0300

    task 285

--- iterating_metodhs_279-286.html_js/iterating_metodhs_279-286.js
+++ iterating_metodhs_279-286.html_js/iterating_metodhs_279-286.js
@@ -62,6 +62,83 @@ arr.forEach(function(elem) {
 console.log(sum5);
 
 //281 потом
+//1
+let numbers = [-2, 5, 1, -5, -1, 1, 3, 4, -1, 11, 7];
+
+let rez = numbers.filter(function(elem) {
+	if (elem >= 0) {
+		return true;
+	} else {
+		return false;
+	}
+});
+
+console.log(rez);
+
+//2
+let rez1 = numbers.filter(function(elem) {
+	if (elem < 0) {
+		return true;
+	} else {
+		return false;
+	}
+});
+
+console.log(rez1);
+
+//3
+
+let rez2 = numbers.filter(function(elem) {
+	if ( elem > 0 &&  elem < 10 ) {
+		return true;
+	} else {
+		return false;
+	}
+});
+
+console.log(rez2);
+
+//4
+
+let str = ['javascript', 'java', 'css', 'html']
+
+let rez3 = str.filter(function(elem) {
+	if ( elem.length > 5 ) {
+		return true;
+	} else {
+		return false;
+	}
+});
+
+console.log(rez3);
+
+//5
+let arr281 = [1, 2, 3, 4, 8];
+
+let rezik = arr281.filter(function(elem, index) {
+	if (elem * index <30) {
+		return true;
+	} else {
+		return false;
+	}
+});
+
+console.log(rezik);
+
+//6
+let arr281_6 = [1, 2, [3, 4], 5, [6, 7]];
+
+let rezik1 = arr281_6.filter(function(elem, index) {
+	if (!Array.isArray(elem)) {
+		return true;
+	} else {
+		return false;
+	}
+});
+
+console.log(rezik1);
+
+
 
 //282
 //1
@@ -108,3 +185,34 @@ let result = array283.some(function(elem,index){
 	return elem * index > 30;
 })
 console.log(result)
+
+//284 
+
+//find найдёт первый подходящий элемент под условие соотвественно ластфайд найдёт последний 
+let result1 = array283.find(function(elem){
+	return elem > 3;
+})
+console.log(result1)
+
+//index напишет индекс первого подходящего соотвественно ластиндекс найдёт последний 
+
+let result2 = array283.findIndex(function(elem){
+	return elem > 3;
+})
+console.log(result2)
+
+//285
+/*массив.reduce(function(промежуточный результат, элемент, индекс, массив)
+
+Промежуточный результат - это переменная, в которой будет накапливаться то значение, 
+
+которое вернет метод reduce, когда переберет все элементы массива*/
+
+let arra = [1, 2, 3, 4, 5, 6];
+
+let ress = arra.reduce(function(sum, elem) {
+	return sum + elem;
+}, 0);
+
+console.log(ress);//ReduceRight перебирает справа налево
+
