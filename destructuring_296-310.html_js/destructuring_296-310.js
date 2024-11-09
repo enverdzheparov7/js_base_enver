357119496ef638a45a24362ebc2d7fe80f71749b
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-09 20:43:33 +0300

    task 300

--- destructuring_296-310.html_js/destructuring_296-310.js
+++ destructuring_296-310.html_js/destructuring_296-310.js
@@ -49,3 +49,25 @@ console.log('-------------------------------------');
  console.log(department);
  console.log(position);
  console.log(salary);
+
+
+ //298
+
+ let arr3 = ['John', 'Smit', 'development', 'programmer', 2000];
+
+ [, surname, department ,,] = arr3;
+
+ console.log('-------------------------------------');
+ console.log(surname);
+ console.log(department); 
+
+
+ //300
+
+ let arr4 = ['John', 'Smit', 'development', 'programmer', 2000];
+
+ [name, surname, ...lasts] = arr4;
+
+ console.log(name);
+ console.log(surname);
+ console.log(lasts);
