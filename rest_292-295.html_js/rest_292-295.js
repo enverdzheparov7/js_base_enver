6460b9b8434359f0a92fb33ff0b580ee2ef05da3
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-09 19:01:39 +0300

    выполнено

--- rest_292-295.html_js/rest_292-295.js
+++ rest_292-295.html_js/rest_292-295.js
@@ -1,4 +1,4 @@
-//292
+//293
 
 function average(...numbers) {
     if (numbers.length === 0) return 0;
@@ -10,3 +10,24 @@ function average(...numbers) {
 console.log(average(1, 2, 3, 4, 5)); 
 console.log(average(10, 20, 30)); 
 console.log(average()); 
+
+//294
+
+let result = unite([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+console.log(result); // выведет [ [1, 2, 3,] [4, 5, 6], 
+//	[7, 8, 9] ] //unite сливает в двумерный массив merge в один массив
+
+
+function unite(...arr) {
+	return arr;
+}
+
+
+//295
+
+let results = merge([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+console.log(results);//[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+function merge(...arrs) {
+	return [].concat(...arrs);
+}
