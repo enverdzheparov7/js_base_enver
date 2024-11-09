bd8d2e11e8122d60a37c624cbde706324dcf52f6
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-09 20:27:53 +0300

    task 297

---
+++ destructuring_296-310.html_js/destructuring_296-310.js
@@ -0,0 +1,51 @@
+//296
+/* В следующем коде части массива записываются в соответствующие переменные:
+
+let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+
+let name       = arr[0];
+let surname    = arr[1];
+let department = arr[2];
+let position   = arr[3];
+let salary     = arr[4];
+Переделайте этот код через деструктуризацию согласно изученной теории.*/
+
+//296
+let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+
+let [name, surname, department, position, salary] = arr ;
+
+console.log(name);
+console.log(surname);
+console.log(department);
+console.log(position);
+console.log(salary);
+
+
+//297
+
+/*function func() {
+	return ['John', 'Smit', 'development', 'programmer', 2000];
+}
+
+let arr = func();
+
+let name       = arr[0];
+let surname    = arr[1];
+let department = arr[2];
+let position   = arr[3];
+let salary     = arr[4];*/
+
+//297
+function func() {
+	return ['John', 'Smit', 'development', 'programmer', 2000];
+}
+
+ [name, surname, department, position, salary] = func() ;
+
+console.log('-------------------------------------');
+ console.log(name);
+ console.log(surname);
+ console.log(department);
+ console.log(position);
+ console.log(salary);
