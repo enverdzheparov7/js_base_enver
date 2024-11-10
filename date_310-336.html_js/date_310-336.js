fdaa8aa10f14ab2df173fb5fae9091e2ee95a2d5
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-10 23:13:37 +0300

    task 320

--- date_310-336.html_js/date_310-336.js
+++ date_310-336.html_js/date_310-336.js
@@ -120,3 +120,98 @@ let bday = new Date(2006, 7, 3);
 let different = now.getTime() - bday.getTime();
 console.log(Math.floor(different/(1000 * 60 * 60 * 24))+ ' дней')
 
+console.log('-------------------------------------319');
+
+//319
+
+let date319 = new Date(2010, 1, 15, 0, 0, 0);
+
+let date319_1 = new Date(2000, 8, 1, 0, 0, 0);
+
+let differ = date319 - date319_1
+
+console.log((differ + ' миллисекунд'))
+console.log(Math.floor(differ/ (1000 * 60 * 60 * 24))+ ' дней')
+console.log(Math.floor(differ/ (1000 * 60 * 60 * 24 * 30.41 ))+ ' месяцев')
+console.log(Math.floor(differ/ (1000 * 60 * 60 * 24 * 30.41 * 12))+ ' лет')
+
+
+console.log('-------------------------------------320');
+
+//320
+
+
+//1
+
+
+
+let date4 = new Date(2018, 1, 35); //4 февраля 2018
+console.log(date4); // какая дата будет?
+//2
+
+
+
+let date5 = new Date(2018, 15, 1); //1 февраля 2019
+console.log(date5); // какая дата будет?
+//3
+
+
+
+let date6 = new Date(2018, 3, 31);//1 мая 2018
+console.log(date6); // какая дата будет?
+//4
+
+
+
+let date7 = new Date(2018, 1, 31);//3 марта
+console.log(date7); // какая дата будет?
+//5
+
+
+
+let date8 = new Date(2018, 12, 33); //2 февраля
+console.log(date8); // какая дата будет?
+//6
+
+
+
+let date9 = new Date(2018, 33, 33);
+console.log(date9); // какая дата будет? 2020 2 ноября
+//7
+
+
+
+let date10 = new Date(2018, 5, 0);
+console.log(date10); // какая дата будет? 31 мая 2018
+//8
+
+
+
+let date11 = new Date(2018, 0, 0);
+console.log(date11); // какая дата будет? 31 декабря 2017
+//9
+
+
+
+let date12 = new Date(2018, -12, -33);
+console.log(date12); // какая дата будет? 28 ноября 2016
+//10
+
+
+
+let date13 = new Date(2018, 0, 1, 23, 0, 60);
+console.log(date13); // какая дата будет? 23:01:00 2018 год 1 января
+
+//11
+
+
+
+let date14 = new Date(2018, 0, 1, 23, 60, 0);
+console.log(date14); // какая дата будет? 2 января 2018 00:00:00
+//12
+
+
+
+let date15 = new Date(2018, 0, 1, 100, 100, 100);
+console.log(date15); // какая дата будет? 5 января 2018 05:41:40
+
